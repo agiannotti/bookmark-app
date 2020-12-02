0f311440d8f491d26e9444de71b3a1e79e246211
Anthony <aj.gianno@gmail.com>
2020-12-01 19:11:54 -0600

    font!

--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
-        ]
-      }
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ]
   }
 };
